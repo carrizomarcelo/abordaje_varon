b7c5743fbf5f030ff4cac8ae5ca9c1988c29abbc
Marcelo Carrizo <carrizomarcelo@gmail.com>
2022-10-24 13:03:35 -0300

    modificada base de datos

--- av/av/settings.py
+++ av/av/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'av.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'av_test',
-        'USER': 'av',
-        'PASSWORD': '123321',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'av_test',
+        # 'USER': 'av',
+        # 'PASSWORD': '123321',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
