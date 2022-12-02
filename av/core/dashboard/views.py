79f589352ee7f9b4db1f92f2db5368415afe06a5
Marcelo Carrizo <carrizomarcelo@gmail.com>
2022-12-02 08:53:29 -0300

    nose

--- av/core/dashboard/views.py
+++ av/core/dashboard/views.py
@@ -1,10 +1,29 @@
+from itertools import count
 from django.views.generic import TemplateView
+from datetime import datetime
+
+from core.encuesta.models import Encuesta
+from django.db.models.functions import Coalesce
 
 
 class DashboardView(TemplateView):
     template_name = 'dashboard.html'
 
+    def get_graph_encuestas_year_month(self):
+        data = []
+        try:
+            year = datetime.now().year
+            for m in range(1,13):
+        
+                total = Encuesta.objects.filter(fechacreacion__year=year, fechacreacion__month=m)
+                data.append(total)
+        except:
+            pass
+        return data
+
+
     def get_context_data(self, **kwargs):
         contex = super().get_context_data(**kwargs)
         contex['panel'] = 'Panel de Ingreso'
+        contex['graph_encuestas_year_month'] = self.get_graph_encuestas_year_month()
         return contex
